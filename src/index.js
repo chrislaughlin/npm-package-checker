b3c8b8de41fd0652ec13bd6d7db4bcbe211c5716
Chris Laughlin <bluntm@gmail.com>
2019-02-12 22:59:37 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
+  const [reposnse, setReposnse] = useState({});
+
+  useEffect(() => {
+    fetch("/.netlify/functions/hello")
+      .then(res => res.json())
+      .then(res => setReposnse(res));
+  });
+
   return (
     <div className="App">
       <h1>npm package check</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h2> RES: {JSON.stringify(reposnse)} </h2>
     </div>
   );
 }
